67e0609403faec71f9a273b0042174ae42c70dff
Adewumi <owonikokoadewunmi@gmail.com>
2025-03-23 16:43:10 +0100

    Option API

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import NotFound from '../views/NotFound.vue'
+import Jobs from "../views/Jobs/Jobs.vue"
+import JobDetails from "../views/Jobs/JobDetails.vue"
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component:AboutView
+  },
+  {
+    path: '/jobs',
+    name:'Jobs',
+    component:Jobs
+},
+{path:'/jobs:id',
+name:'JobDetails',
+component:JobDetails,
+props:true},
+//redirect
+{path:'/all-jobs',
+redirect:'/jobs'
+},
+// error 404
+{
+  path:'/:catchAll(.*)',
+  name:"NotFound",
+  component: NotFound
+}
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus officia necessitatibus praesentium, explicabo cumque iusto architecto iste placeat nemo quisquam minima asperiores reiciendis sunt debitis nostrum voluptate facilis aperiam ullam eum eos illum? Adipisci odio dolores corporis tempora id officiis laborum, ut voluptatem nulla ipsa vel harum quasi cupiditate aspernatur animi necessitatibus. Cupiditate labore sint suscipit illum nulla ut rerum est culpa maxime animi delectus minima nostrum natus sapiente, temporibus eaque voluptatibus. Cumque sed doloribus velit eius saepe dolore voluptas at iure impedit quis officia, quod perferendis sunt ut id, eligendi inventore ipsum. Ullam maiores doloremque sapiente consequuntur, totam consequatur.</p>
+  </div>
+</template>
